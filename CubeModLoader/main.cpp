3a4d8cabf39d079c06a3c8744941ff21570d9dcc
ChrisMiuchiz <chris000005@gmail.com>
2019-10-15 00:52:28 -0400

    Implement new .fip method of injecting mods

--- CubeModLoader/main.cpp
+++ CubeModLoader/main.cpp
@@ -2,9 +2,16 @@
 #include <windows.h>
 #include <vector>
 #include "DLL.h"
+#include "crc.h"
 
 #define MOD_MAJOR_VERSION 4
 #define MOD_MINOR_VERSION 1
+
+#define CUBE_VERSION "1.0.0-1"
+#define CUBE_PACKED_CRC 0xC7682619
+#define CUBE_UNPACKED_CRC 0xBA092543
+
+#define MODLOADER_NAME "CubeModLoader"
 
 
 #define no_optimize __attribute__((optimize("O0")))
@@ -30,8 +37,12 @@ dllname->name = GetProcAddress(dllname->handle, #name);
 
 using namespace std;
 
-void* base;
-vector <DLL*> modDLLs;
+void* base; // Module base
+vector <DLL*> modDLLs; // Every mod we've loaded
+HMODULE hSelf; // A handle to ourself, to prevent being unloaded
+void* initterm_e; // A pointer to a function which is run extremely soon after starting, or after being unpacked
+const size_t BYTES_TO_MOVE = 14; // The size of a far jump
+char initterm_e_remember[BYTES_TO_MOVE]; // We'll use this to store the code we overwrite in initterm_e, so we can put it back later.
 
 void WriteFarJMP(void* source, void* destination) {
     DWORD dwOldProtection;
@@ -59,55 +70,61 @@ void SetupHandlers() {
     SetupCheckInventoryFullHandler();
 }
 
-void Popup(const char* title, char* msg ){
+void Popup(const char* title, const char* msg ) {
     MessageBoxA(0, msg, title, MB_OK | MB_ICONINFORMATION);
 }
 
-extern "C" __declspec(dllexport) BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
-    switch (fdwReason) {
-    case DLL_PROCESS_ATTACH:
-        base = GetModuleHandle(NULL);
+void PrintLoadedMods() {
+    std::string mods("Mods Loaded:\n");
+    for (DLL* dll : modDLLs) {
+        mods += dll->fileName;
+        mods += "\n";
+    }
+    Popup("Loaded Mods", mods.c_str());
+}
 
-        SetupHandlers();
+// Handles injecting callbacks and the mods
+void StartMods() {
+    char msg[256] = {0};
 
-        //Find mods
-        HANDLE hFind;
-        WIN32_FIND_DATA data;
+    SetupHandlers();
 
-        CreateDirectory("Mods", NULL);
-        hFind = FindFirstFile("Mods\\*.dll", &data);
-        if (hFind != INVALID_HANDLE_VALUE) {
-            do {
-                // We should be loaded into the application's address space, so we can just LoadLibraryA
-                DLL* dll = new DLL(string("Mods\\") + data.cFileName);
-                dll->Load();
-                printf("Loaded %s\n", dll->fileName.c_str());
-                modDLLs.push_back(dll);
-            } while (FindNextFile(hFind, &data));
-            FindClose(hFind);
-        }
+    //Find mods
+    HANDLE hFind;
+    WIN32_FIND_DATA data;
 
-        // Find all the functions the mods may export
-        for (DLL* dll: modDLLs) {
-            MUST_IMPORT(dll, ModMajorVersion);
-            MUST_IMPORT(dll, ModMinorVersion);
-            MUST_IMPORT(dll, ModPreInitialize);
-            IMPORT(dll, ModInitialize);
-            IMPORT(dll, HandleChat);
-            IMPORT(dll, HandleP2PRequest);
-            IMPORT(dll, HandleCheckInventoryFull);
-        }
+    CreateDirectory("Mods", NULL);
+    hFind = FindFirstFile("Mods\\*.dll", &data);
+    if (hFind != INVALID_HANDLE_VALUE) {
+        do {
+            // We should be loaded into the application's address space, so we can just LoadLibraryA
+            DLL* dll = new DLL(string("Mods\\") + data.cFileName);
+            dll->Load();
+            printf("Loaded %s\n", dll->fileName.c_str());
+            modDLLs.push_back(dll);
+        } while (FindNextFile(hFind, &data));
+        FindClose(hFind);
+    }
 
-        // Ensure version compatibility
-        char msg[512] = {0};
-        for (DLL* dll: modDLLs) {
-            int majorVersion = ((int(*)())dll->ModMajorVersion)();
-            int minorVersion = ((int(*)())dll->ModMinorVersion)();
-            if (majorVersion != MOD_MAJOR_VERSION) {
-                sprintf(msg, "%s has major version %d but requires %d.\n", dll->fileName.c_str(), majorVersion, MOD_MAJOR_VERSION);
-                Popup("Error", msg);
-                exit(1);
-            }
+    // Find all the functions the mods may export
+    for (DLL* dll: modDLLs) {
+        MUST_IMPORT(dll, ModMajorVersion);
+        MUST_IMPORT(dll, ModMinorVersion);
+        MUST_IMPORT(dll, ModPreInitialize);
+        IMPORT(dll, ModInitialize);
+        IMPORT(dll, HandleChat);
+        IMPORT(dll, HandleP2PRequest);
+        IMPORT(dll, HandleCheckInventoryFull);
+    }
+
+    // Ensure version compatibility
+    for (DLL* dll: modDLLs) {
+        int majorVersion = ((int(*)())dll->ModMajorVersion)();
+        int minorVersion = ((int(*)())dll->ModMinorVersion)();
+        if (majorVersion != MOD_MAJOR_VERSION) {
+            sprintf(msg, "%s has major version %d but requires %d.\n", dll->fileName.c_str(), majorVersion, MOD_MAJOR_VERSION);
+            Popup("Error", msg);
+            exit(1);
 
             if (minorVersion > MOD_MINOR_VERSION) {
                 sprintf(msg, "%s has minor version %d but requires %d or lower.\n", dll->fileName.c_str(), minorVersion, MOD_MINOR_VERSION);
@@ -126,7 +143,107 @@ extern "C" __declspec(dllexport) BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD
                 ((void(*)())dll->ModInitialize)();
             }
         }
+    }
+    if (hSelf) PrintLoadedMods();
+    return;
+}
+void* StartMods_ptr = (void*)&StartMods;
 
+void no_optimize ASMStartMods() {
+    asm(PUSH_ALL
+        PREPARE_STACK
+
+        // Initialize mods and callbacks
+        "call [StartMods_ptr] \n"
+
+        // We can put initterm_e back how we found it.
+        "call [CopyInitializationBack_ptr] \n"
+
+        RESTORE_STACK
+        POP_ALL
+
+        // Run initterm_e properly this time.
+        "jmp [initterm_e] \n"
+        );
+}
+
+void PatchFreeImage(){
+    DWORD oldProtect;
+    void* patchaddr = (void*)GetModuleHandleA("FreeImage.dll") + 0x1E8C12;
+    VirtualProtect((LPVOID)patchaddr, 8, PAGE_EXECUTE_READWRITE, &oldProtect);
+    *(uint64_t*)patchaddr = 0x909090000000A8E9;
+}
+
+void InitializationPatch() {
+    // Get pointer to initterm_e
+    initterm_e = *(void**)(base + 0x42CBD8);
+
+    // Store old code, we'll copy it back once we regain control.
+    memcpy(initterm_e_remember, initterm_e, BYTES_TO_MOVE);
+
+    // Write a jump to our code
+    WriteFarJMP(initterm_e, (void*)&ASMStartMods);
+}
+
+// This restores initterm_e to how it was before we hijacked it.
+void CopyInitializationBack() {
+    DWORD dwOldProtection;
+    VirtualProtect(initterm_e, BYTES_TO_MOVE, PAGE_EXECUTE_READWRITE, &dwOldProtection);
+
+    memcpy(initterm_e, initterm_e_remember, BYTES_TO_MOVE);
+
+    VirtualProtect(initterm_e, BYTES_TO_MOVE, dwOldProtection, &dwOldProtection);
+
+    return;
+}
+void* CopyInitializationBack_ptr = (void*)&CopyInitializationBack;
+
+bool already_ran = false;
+extern "C" __declspec(dllexport) BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
+    switch (fdwReason) {
+    case DLL_PROCESS_ATTACH:
+        base = GetModuleHandle(NULL);
+
+        // Don't allow this to run more than once
+        if (already_ran)
+            return 1;
+
+        already_ran = true;
+
+        char msg[256] = {0};
+
+        // This serves to prevent ourself from being unloaded, if we are a .fip
+        hSelf = LoadLibrary(MODLOADER_NAME ".fip");
+
+        // The user could also inject this as a DLL, so if we can't find our .fip, we were probably launched with a launcher.
+        // Therefore, we don't need to prompt the user.
+        if (hSelf) {
+            if (MessageBoxA(NULL, "Would you like to run with mods?", "Cube World Mod Loader", MB_YESNO) != IDYES) {
+                PatchFreeImage();
+                return true;
+            }
+        }
+
+        base = GetModuleHandle(NULL);
+
+
+        // Figure out where the executable is and verify its checksum
+        char cubePath[_MAX_PATH+1];
+        GetModuleFileName(NULL, cubePath, _MAX_PATH);
+
+        uint32_t checksum = crc32_file(cubePath);
+        if (checksum == CUBE_PACKED_CRC || checksum == CUBE_UNPACKED_CRC) {
+            // Patch some code to run StartMods. This method makes it work with AND without SteamStub.
+            InitializationPatch();
+        } else {
+            sprintf(msg, "%s does not seem to be version %s. CRC %08X", cubePath, CUBE_VERSION, checksum);
+            Popup("Error", msg);
+            PatchFreeImage();
+            return true;
+        }
+
+        Sleep(250);
+        PatchFreeImage();
     }
     return true;
 }
