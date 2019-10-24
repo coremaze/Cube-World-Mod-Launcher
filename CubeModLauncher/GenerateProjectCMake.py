acb4ce7729ea92219215b599cf38d670681e3a2d
ChrisMiuchiz <chris000005@gmail.com>
2019-10-24 18:14:14 -0400

    cling clong

---
+++ CubeModLauncher/GenerateProjectCMake.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import os
+
+path = os.path.dirname(os.path.realpath(__file__))
+_, folderName = os.path.split(path)
+
+files = list(Path(path).glob('*.cpp'))
+   
+for filename in files:
+    print(filename)
+
+lines = [
+'cmake_minimum_required (VERSION 3.8)',
+'project(project_%s)' % folderName,
+'add_executable (%s \n    %s)' % (folderName, '\n    '.join([str(x)[len(path)+1:].replace('\\', '/') for x in files]))
+    ]
+
+with open(os.path.join(path, 'CMakeLists.txt'), 'w') as f:
+    f.write('\n'.join(lines))
